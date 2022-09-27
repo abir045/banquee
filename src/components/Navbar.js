01f649a2e95d0f6b3b319a7c5abd3a8b0105f6d4
Salekin Imran <abir045@gmail.com>
2022-09-27 14:13:28 +0600

    updated navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "../images/Vector.svg";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex justify-around items-center h-[91px]">
+        <a href="/">
+          <h1 className="flex font-bold text-[#5BB5A2] text-[32px] tracking-[-0.03em]">
+            banquee.
+          </h1>
+        </a>
+        <ul className="flex space-x-5 font-medium text-base text-[#1A191E]">
+          <li>
+            <a href="/features">Features</a>
+          </li>
+          <li className="flex space-x-2">
+            <a href="/compare">Compare</a>
+            <img src={logo} alt="logo" />
+          </li>
+          <li>
+            <a href="/support">Support</a>
+          </li>
+          <li className="flex space-x-2">
+            <a href="/blog">Blog</a>
+            <img src={logo} alt="logo" />
+          </li>
+        </ul>
+
+        <div className="flex space-x-7 font-bold items-center">
+          <button className="text-[#5BB5A2] text-lg leading-[18px] font-bold tracking-[-0.03em]">
+            Login
+          </button>
+          <button className="text-[15px] font-medium bg-[#5BB5A2] text-white p-3 rounded-[6px]">
+            Open Account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
